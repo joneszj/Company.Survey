074a62f2bcd953d3fb51946d44c18cd3172b66ab
joneszj <joneszj@gmail.com>
2020-06-17 18:57:35 -0400

    begin questionnaire admin ctrl & view

---
+++ Company.Survey.Admin/Controllers/QuestionnaireController.cs
@@ -0,0 +1,27 @@
+﻿using Company.Survey.Core.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Company.Survey.Admin.Controllers
+{
+    public class QuestionnaireController : Controller
+    {
+        private readonly CoreContext _context;
+
+        public QuestionnaireController(CoreContext context)
+        {
+            _context = context;
+        }
+
+        public ActionResult Details(int surveyId)
+        {
+            var questionnaire = _context.Surveys.Where(e => e.Id == surveyId)
+                .Include(e=>e.SurveySteps)
+                    .ThenInclude(e=>e.QuestionGroups)
+                .Include(e=>e.SurveySteps)
+                    .ThenInclude(e=>e.Questions);
+            return View("Questionnaire/Edit", questionnaire);
+        }
+    }
+}
